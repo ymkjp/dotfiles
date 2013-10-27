329674fbc0ea418d6d4595e307b087b36951d2f1
Kenta YAMAMOTO <lq33xxx@gmail.com>
2013-10-28 00:03:31 +0900

    Remove tmux-powerline

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOT_FILES=`cat ${HOME}/dotfiles/.gitignore | awk '$1 ~ /^\!/ { print $1 }' | sed -e "s/\!\///g" | tr '\n' ' '`
+
+for FILE in ${DOT_FILES[@]}
+do
+    ln -Fins ${HOME}/dotfiles/${FILE} ${HOME}
+done
+
+# install oh-my-zsh
+[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
