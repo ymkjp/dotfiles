fe5943dadd4d671bbba2a3153b4e2173b3ecf055
Kenta YAMAMOTO <i05ymkjp@gmail.com>
2013-10-28 01:01:21 +0900

    Fix process of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=`cat ${HOME}/dotfiles/.gitignore | awk '$1 ~ /^\!/ { print $1 }' | sed -e "s/\!\///g" | tr '\n' ' '`
+DOT_FILES=`cat ${HOME}/dotfiles/.gitignore | awk '$1 ~ /^\!/ { print $1 }' | sed -e "s/\!\///g" | sed -e "/setup.sh/d" | tr '\n' ' '`
 
 for FILE in ${DOT_FILES[@]}
 do
