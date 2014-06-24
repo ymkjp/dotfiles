5675b9f2c85a69f690c360828485ebed6e189d62
ymkjp <i05ymkjp@gmail.com>
2014-06-24 09:57:30 +0900

    Added feature to delete trailing whitespace in git pre-commit

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-DOT_FILES_PATH="${HOME}/dotfiles"
-DOT_FILES=`cat ${DOT_FILES_PATH}/.gitignore | awk '$1 ~ /^\!/ { print $1 }' | sed -e "s/\!\///g" | sed -e "/setup.sh/d" | tr '\n' ' '`
+ROOT_PATH="${HOME}/dotfiles"
+DOT_FILES=`cat ${ROOT_PATH}/.gitignore | awk '$1 ~ /^\!/ { print $1 }' | sed -e "s/\!\///g"`
+TARGET_FILES=`echo -e "${DOT_FILES}" | sed -e "/setup.sh/d" -e "/git_hooks/d" | tr '\n' ' '`
 LOCAL_DOT_FILES=".zshrc.local .gitconfig.local"
 
-for FILE in ${DOT_FILES[@]}
+for FILE in ${TARGET_FILES[@]}
 do
-    ln -is ${DOT_FILES_PATH}/${FILE} ${HOME}
+    ln -is ${ROOT_PATH}/${FILE} ${HOME}
 done
 
 for FILE in ${LOCAL_DOT_FILES[@]}
 do
-    [[ ! -e ${DOT_FILES_PATH}/${FILE} ]] && touch ${DOT_FILES_PATH}/${FILE}
-    ln -is ${DOT_FILES_PATH}/${FILE} ${HOME}
+    [[ ! -e ${ROOT_PATH}/${FILE} ]] && touch ${ROOT_PATH}/${FILE}
+    ln -is ${ROOT_PATH}/${FILE} ${HOME}
 done
 
 if type zsh > /dev/null 2>&1; then
@@ -20,3 +21,7 @@ if type zsh > /dev/null 2>&1; then
     [[ ! -d ~/.oh-my-zsh ]] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
+# Delete trailing whitespace by git pre-commit hooks
+# http://qiita.com/k0kubun/items/8f6d7eded1d833187449
+# In .git/hooks/pre-commit
+# . .git_hooks/delete_trailing_whitespace
