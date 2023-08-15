cab7454133d7d596f31dcea61502b3f70993b990
Kenta Yamamoto <kyamamoto@atlassian.com>
2023-08-15 13:27:10 +0900

    Updated PROMPT

--- .zshrc.custom.sh
+++ .zshrc.custom.sh
@@ -134,7 +134,7 @@ setopt transient_rprompt
 unsetopt promptcr
 # https://linux.die.net/man/1/zshmisc
 NEWLINE=$'\n'
-PROMPT="%F{8}%D{%FT%T%z} %(?.%?.%S%?%s)%f${NEWLINE}${PROMPT}"
+PROMPT="%F{8}%D{%{F}T%T%z} %(?.%?.%S%?%s)%f${NEWLINE}${PROMPT}"
 
 # === History ===
 # http://news.mynavi.jp/column/zsh/003/index.html
